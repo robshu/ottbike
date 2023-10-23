4ca3336cd7c3551bebe2a5478d8da8996fefa91b
robshu <robshudra@gmail.com>
2023-10-23 13:56:01 -0400

    gl update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line no-unused-vars
 const config = {
-  style: 'mapbox://styles/robshu/cl48p9qvh000h14oeglbujald',
+  style: 'mapbox://styles/robshu/cl4hp53wp002f16s2hnd4coqu',
   accessToken:
     'pk.eyJ1Ijoicm9ic2h1IiwiYSI6ImNsNDhvcjQ5azB1OWIzZG5zazB1OG42bHMifQ.3zcPiI2Yp0B1Sdk27XH5mg',
   CSV: 'https://docs.google.com/spreadsheets/d/13l93xO1tm6AcuorVuIsAIt4dLTQ-oFFG05CscA9uxOw/gviz/tq?tqx=out:csv&sheet=Intersections',
